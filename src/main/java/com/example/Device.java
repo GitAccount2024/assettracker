6ff7e1739b36698f7f584812522c4daec4be893f
DevNeo <onlinexneo@icloud.com>
2026-05-22 20:37:57 +0200

    Device-Modellklasse hinzugefügt

---
+++ src/main/java/com/example/Device.java
@@ -0,0 +1,50 @@
+package com.example;
+
+public class Device {
+
+    private Long id;
+    private String name;
+    private String type;
+    private String status;
+
+    public Device() {
+    }
+
+    public Device(String name, String type, String status) {
+        this.name = name;
+        this.type = type;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
